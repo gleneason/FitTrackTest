11d106d2c4ea6c01b9f2fff8355f611f53223663
gleneason <gleneason@yahoo.com>
2026-02-23 02:16:36 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
-const CACHE_NAME = "glen-track-v1";
+// Simple offline cache for Glen Track (PWA)
+const CACHE = "glen-track-cache-v1";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((hit) => hit || fetch(event.request))
   );
 });
